c4ec81c476cc3c86064481db6e06a4fa244a0cad
SilverRat <jcgray@yahoo.com>
2017-08-18 12:40:25 -0700

    Workin on auth

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ var config = require("config");
 winston.level = config.get("AJ.log.level");
 
 var express = require("express");
+var passport = require('passport');
+var Strategy = require('passport-facebook').Strategy;
 var favicon = require("serve-favicon");
 var morgan = require("morgan");
 var bodyParser = require("body-parser");
@@ -15,6 +17,28 @@ var path = require("path");
 var routes = ["/journalEntry","/tank"];
 var idRoutes = ["/journalEntry/:id","/tank/:id"];
 
+passport.use(new Strategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: 'http://localhost:3000/login/facebook/return'
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    // In this example, the user's Facebook profile is supplied as the user
+    // record.  In a production-quality application, the Facebook profile should
+    // be associated with a user record in the application's database, which
+    // allows for account linking and authentication with other identity
+    // providers.
+    return cb(null, profile);
+  }));
+  
+  passport.serializeUser(function(user, cb) {
+    cb(null, user);
+  });
+  
+  passport.deserializeUser(function(obj, cb) {
+    cb(null, obj);
+  });
+
 var app = express();
 
 app.set("port", process.env.PORT || 3000);
@@ -23,7 +47,9 @@ app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, "client")));
-//app.use(dataValidation.validate());
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 var server = http.createServer(app);
 
@@ -84,7 +110,7 @@ app.put(idRoutes, updateData);
 //  record types.
 
 //Journal Entries - Water Chemistry tests.
-app.get("/journalEntries", function(req, res) {
+app.get("/journalEntries", require('connect-ensure-login').ensureLoggedIn(), function(req, res) {
     winston.info("Querying for journal entries.");
     ajDbApi.getJournalEntries("journalEntry").then(function(docs){
         winston.info("Winston - Getting journal entries:", docs);
@@ -107,4 +133,22 @@ app.get("/tanks", function(req, res) {
     });
 });
 
+app.get('/login',
+function(req, res){
+  res.render('login');
+});
 
+app.get('/login/facebook',
+passport.authenticate('facebook'));
+
+app.get('/login/facebook/return', 
+passport.authenticate('facebook', { failureRedirect: '/login' }),
+function(req, res) {
+  res.redirect('/');
+});
+
+app.get('/profile',
+require('connect-ensure-login').ensureLoggedIn(),
+function(req, res){
+  res.render('profile', { user: req.user });
+});
