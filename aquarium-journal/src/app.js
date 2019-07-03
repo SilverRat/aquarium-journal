3598f4afd44c819d50c3823be644dade92313592
Jerry Gray <GrayJe@SacCounty.net>
2019-07-03 11:54:58 -0700

    refactor: converted from durandal to aurelia
    Converted some of the basic classes and components from durandal to aurelia.

--- aquarium-journal/src/app.js
+++ aquarium-journal/src/app.js
@@ -1,3 +1,13 @@
+import { PLATFORM } from 'aurelia-pal';
+
 export class App {
-  message = 'Hello World!';
+  configureRouter(config, router) {
+    config.title = 'Aquarium Journal';
+    config.map([
+      { route: "", title: "Welcome", moduleId: PLATFORM.moduleName("welcome/welcome"), nav: true },
+      { route: "Journal Entries", moduleId: PLATFORM.moduleName("journal-entries/journalEntries"), nav: true },
+      { route: "Tanks", moduleId: PLATFORM.moduleName("tanks/tanks"), nav: true }
+    ]);
+    this.router = router;
+  }
 }
